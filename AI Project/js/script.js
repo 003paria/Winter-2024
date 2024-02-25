60314a694dd644bb839f180343ed662ec40fcd0d
003paria <003paria.j@gmail.com>
2024-02-25 10:19:08 -0500

    starting out, bg image, fixed canvas

---
+++ AI Project/js/script.js
@@ -0,0 +1,37 @@
+/**
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+let backgroundImage;
+
+
+/**
+Description of preload
+*/
+function preload() {
+  backgroundImage = loadImage('assets/images/intro.jpg');
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+  // Create a fixed canvas 
+  createCanvas(1200, 570);
+  // Set the background to the into image 
+  background(backgroundImage);
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
