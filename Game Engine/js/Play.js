f5fea4881626048340680647c9414d934e43d78f
003paria <003paria.j@gmail.com>
2024-03-15 11:21:39 -0400

    Setting up Play.js

---
+++ Game Engine/js/Play.js
@@ -0,0 +1,30 @@
+/**
+
+Explain the game 
+
+*/
+
+
+class Play extends Phaser.Scene {
+
+  /**
+  Just sets the scene's key name
+  */
+  constructor() {
+    super({
+      key: `play`
+    });
+  }
+
+  // Called when the scene start
+  create() {
+
+  }
+
+  // Called every frame
+  update() {
+
+  }
+
+}
+
