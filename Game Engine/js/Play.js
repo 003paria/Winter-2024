d69eec1bd7a97440821717c7b183b2417a7ba089
003paria <003paria.j@gmail.com>
2024-03-16 09:18:35 -0400

    Able to "destroy" bug when I click on them

--- Game Engine/js/Play.js
+++ Game Engine/js/Play.js
@@ -19,55 +19,52 @@ class Play extends Phaser.Scene {
   create() {
     // Add background image
     this.add.image(0, 0, 'background').setOrigin(0);
-
-  // Create a group of bugs with some basic physics configuration
-  this.bugs = this.physics.add.group({
-    // Image key to use
-    key: 'bug',
-    // How many
-    quantity: 2,
-    // Collide with the "walls"
-    // collideWorldBounds: true,
-    // Set the initial velocity of the bugs to move towards the right
-    velocityX: 100, // Adjust velocity as needed
-    // How much to they bounce when they hit something?
-    bounceX: 1, // Ensure full bounce
-    bounceY: 1
-  });
-
-
+    // Create a group of bugs with some basic physics configuration
+    this.bugs = this.physics.add.group({
+      // Image key to use
+      key: 'bug',
+      // How many
+      quantity: 2,
+      // Collide with the "walls"
+      // collideWorldBounds: true,
+      // Set the initial velocity of the bugs to move towards the right
+      velocityX: 100, // Adjust velocity as needed
+      // How much to they bounce when they hit something?
+      bounceX: 1, // Ensure full bounce
+      bounceY: 1
+    });
   // Initialize score
     this.score = 0;
     this.scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#FFF' });
-    
     // Create a timer event to spawn bugs every second
     this.timerEvent = this.time.addEvent({ delay: 1000, callback: this.spawnBug, callbackScope: this, loop: true });
 
     // Listen for mouse clicks to smash bugs
     this.input.on('pointerdown', this.smashBug, this);
+    console.log();
   }
   
   // Function to spawn a bug
   spawnBug() {
   // Calculate the Y position randomly within the game height
   let y = Phaser.Math.Between(50, this.game.config.height - 50); // Adjust Y range as needed
-
   // Create a bug at the far left edge of the screen with the random Y position
   this.bug = this.bugs.create(0, y, 'bug');
   }
 
   // Called when a bug is smashed
   smashBug(pointer) {
-    // Check if any bugs were clicked
-    let clickedBugs = this.physics.overlapPoint(pointer.x, pointer.y, this.bugs.getChildren());
-    if (clickedBugs.length > 0) {
-      // Destroy the first bug clicked
-      clickedBugs[0].destroy();
-      // Update the score
-      this.score += 1;
-      this.scoreText.setText('Score: ' + this.score);
-    }
-  }
+    this.bugs.children.iterate(bug => {
+        if (bug && bug.getBounds && Phaser.Geom.Rectangle.ContainsPoint(bug.getBounds(), pointer)) {
+            bug.destroy();
+            this.score += 1;
+            this.scoreText.setText('Score: ' + this.score);
+        }
+    });
+    console.log();
+}
+
+
 
   // Called every frame
   update() {
